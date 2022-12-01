42bdec043ff7951200f730db63467acb572cc562
Wendel <wendelsilva.lopes10@gmail.com>
2022-12-01 14:57:07 -0300

    3-listas

--- main.js
+++ main.js
@@ -1,9 +1,100 @@
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//Som Pom.............................................................
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+listaDeTeclas[0].onclick = tocaSomPom;
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*  VERSÃO ESTENDIDA DO CODIGO
+
 function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
 
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+//Som Clap ........................................................
 function tocaSomClap() {
     document.querySelector('#som_tecla_clap').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+
+//Som Tim .........................................................
+
+function tocaSomTim() {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+
+
+//Som Puff .........................................................
+function tocaSomPuff() {
+    document.querySelector('#som_tecla_puff').play();
+
+}
+
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+
+
+//Som Splash..............................................................
+function tocaSomSplash() {
+    document.querySelector('#som_tecla_splash').play();
+
+}
+
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
+
+//Som Toim ...........................................................
+
+function tocaSomToim() {
+    document.querySelector('#som_tecla_toim').play();
+
+}
+
+document.querySelector('.tecla_toim').onclick = tocaSomToim;
+
+
+//Som Psh.............................................................
+function tocaSomPsh() {
+    document.querySelector('#som_tecla_psh').play();
+
+}
+
+document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+
+
+//Som Tic ............................................................
+function tocaSomTic() {
+    document.querySelector('#som_tecla_tic').play();
+
+}
+
+document.querySelector('.tecla_tic').onclick = tocaSomTic;
+
+//Som Tom ............................................................
+function tocaSomTom() {
+    document.querySelector('#som_tecla_tom').play();
+
+}
+
+
+document.querySelector('.tecla_tom').onclick = tocaSomTom; */
